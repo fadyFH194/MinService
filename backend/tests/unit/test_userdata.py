d095bd474a4326e56fb69058de716800b9b7df83
fadyFH194 <131557935+fadyFH194@users.noreply.github.com>
2024-11-19 23:38:52 +0100

    test fix2

--- backend/tests/unit/test_userdata.py
+++ backend/tests/unit/test_userdata.py
@@ -48,7 +48,6 @@ def test_get_nuser_data(client, test_user):
 
         # Validate user data
         assert data["name"] == "Test User"
-        assert data["email"] == "testuser@example.com"
         assert data["about"] == "This is a test user"  # Ensure this matches test setup
         assert data["classBatch"] == "2024"
         assert data["currentLocation"] == "Test City"
